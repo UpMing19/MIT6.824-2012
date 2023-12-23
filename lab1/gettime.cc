65fc6e7dda2c9e4d1189118a7b79e51fbe0cbe64
DaMing <dmacmer@163.com>
2023-12-23 18:08:30 +0000

    update on macOS

--- lab1/gettime.cc
+++ lab1/gettime.cc
@@ -1,65 +1,8 @@
-/*
- * Copyright (c), MM Weiss
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- *     1. Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *
- *     2. Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
- *
- *     3. Neither the name of the MM Weiss nor the names of its contributors
- *     may be used to endorse or promote products derived from this software without
- *     specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
- *  clock_gettime_stub.c
- *  gcc -Wall -c clock_gettime_stub.c
- *  posix realtime functions; MacOS user space glue
- */
-
-/*  @comment
- *  other possible implementation using intel builtin rdtsc
- *  rdtsc-workaround: http://www.mcs.anl.gov/~kazutomo/rdtsc.html
- *
- *  we could get the ticks by doing this
- *
- *  __asm __volatile("mov %%ebx, %%esi\n\t"
- *  		"cpuid\n\t"
- *  		"xchg %%esi, %%ebx\n\t"
- *  		"rdtsc"
- *  		: "=a" (a),
- *  		  "=d" (d)
- * 	);
-
- *  we could even replace our tricky sched_yield call by assembly code to get a better accurency,
- *  anyway the following C stub will satisfy 99% of apps using posix clock_gettime call,
- *  moreover, the setter version (clock_settime) could be easly written using mach primitives:
- *  http://www.opensource.apple.com/source/xnu/xnu-${VERSION}/osfmk/man/ (clock_[set|get]_time)
- *
- *  hackers don't be crackers, don't you use a flush toilet?
- *
- *
- *  @see draft: ./posix-realtime-stub/posix-realtime-stub.c
- *
- */
-
 #ifdef __APPLE__
+#include <Availability.h>
+
+// 只在 macOS 版本低于 10.12 时定义
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_12
 
 #pragma weak clock_gettime
 
@@ -72,13 +15,15 @@
 #include <unistd.h>
 #include <sched.h>
 
-typedef enum
-{
-	CLOCK_REALTIME,
-	CLOCK_MONOTONIC,
-	CLOCK_PROCESS_CPUTIME_ID,
-	CLOCK_THREAD_CPUTIME_ID
+typedef enum {
+    CLOCK_REALTIME,
+    CLOCK_MONOTONIC,
+    CLOCK_PROCESS_CPUTIME_ID,
+    CLOCK_THREAD_CPUTIME_ID
 } clockid_t;
+
+
+
 
 static mach_timebase_info_data_t __clock_gettime_inf;
 
@@ -144,5 +89,8 @@ int clock_gettime(clockid_t clk_id, struct timespec *tp)
 	}
 	return retval;
 }
+
+
+#endif // __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_12
 
 #endif // __APPLE__
--- lab1/gettime.h
+++ lab1/gettime.h
@@ -1,3 +1,4 @@
+/*
 #ifndef gettime_h
 #define gettime_h
 
@@ -14,3 +15,28 @@ int clock_gettime(clockid_t clk_id, struct timespec *tp);
 #endif
 
 #endif
+*/
+
+#ifndef gettime_h
+#define gettime_h
+
+#ifdef __APPLE__
+#include <Availability.h>
+
+// 只有在目标 macOS 版本低于 10.12 版本的情况下才定义
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_12
+
+typedef enum {
+    CLOCK_REALTIME,
+    CLOCK_MONOTONIC,
+    CLOCK_PROCESS_CPUTIME_ID,
+    CLOCK_THREAD_CPUTIME_ID
+} clockid_t;
+
+int clock_gettime(clockid_t clk_id, struct timespec *tp);
+
+#endif // __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_12
+
+#endif // __APPLE__
+
+#endif // gettime_h
