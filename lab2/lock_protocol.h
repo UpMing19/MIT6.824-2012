cbae5d2ff539e96f1984d26b88896968d3f9e5ec
DaMing <63656745+UpMing19@users.noreply.github.com>
2024-01-05 10:23:08 +0000

    init lab2

---
+++ lab2/lock_protocol.h
@@ -0,0 +1,42 @@
+// lock protocol
+
+#ifndef lock_protocol_h
+#define lock_protocol_h
+
+#include "rpc.h"
+
+class lock_protocol {
+ public:
+  enum xxstatus { OK, RETRY, RPCERR, NOENT, IOERR };
+  typedef int status;
+  typedef unsigned long long lockid_t;
+  typedef unsigned long long xid_t;
+  enum rpc_numbers {
+    acquire = 0x7001,
+    release,
+    stat
+  };
+};
+
+class rlock_protocol {
+ public:
+  enum xxstatus { OK, RPCERR };
+  typedef int status;
+  enum rpc_numbers {
+    revoke = 0x8001,
+    retry = 0x8002
+  };
+};
+
+class lock {
+ public: 
+    enum lock_status {FREE, LOCKED};
+	lock_protocol::lockid_t lid;
+	int status;
+	pthread_cond_t lcond;
+														      
+	lock(lock_protocol::lockid_t);
+	lock(lock_protocol::lockid_t, int);
+	~lock(){};
+};
+#endif 
