de3e86ac5dbc45a337c6be9326cfbcb810da87bf
DaMing <dmacmer@163.com>
2024-01-06 23:33:45 +0000

    init lab4

--- lab4/lock_client_cache.cc
+++ lab4/lock_client_cache.cc
@@ -8,15 +8,12 @@
 #include <stdio.h>
 #include "tprintf.h"
 
-
 int lock_client_cache::last_port = 0;
 
-lock_client_cache::lock_client_cache(std::string xdst, 
-				     class lock_release_user *_lu)
-  : lock_client(xdst), lu(_lu)
+lock_client_cache::lock_client_cache(std::string xdst, class lock_release_user *_lu) : lock_client(xdst), lu(_lu)
 {
-  srand(time(NULL)^last_port);
-  rlock_port = ((rand()%32000) | (0x1 << 10));
+  srand(time(NULL) ^ last_port);
+  rlock_port = ((rand() % 32000) | (0x1 << 10));
   const char *hname;
   // VERIFY(gethostname(hname, 100) == 0);
   hname = "127.0.0.1";
@@ -40,11 +37,10 @@ lock_protocol::status
 lock_client_cache::release(lock_protocol::lockid_t lid)
 {
   return lock_protocol::OK;
-
 }
 
 rlock_protocol::status
-lock_client_cache::revoke_handler(lock_protocol::lockid_t lid, 
+lock_client_cache::revoke_handler(lock_protocol::lockid_t lid,
                                   int &)
 {
   int ret = rlock_protocol::OK;
@@ -52,12 +48,9 @@ lock_client_cache::revoke_handler(lock_protocol::lockid_t lid,
 }
 
 rlock_protocol::status
-lock_client_cache::retry_handler(lock_protocol::lockid_t lid, 
+lock_client_cache::retry_handler(lock_protocol::lockid_t lid,
                                  int &)
 {
   int ret = rlock_protocol::OK;
   return ret;
 }
-
-
-
