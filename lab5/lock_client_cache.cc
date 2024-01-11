f989353bfadc34469826e5c0c48772ab8075b2c1
DaMing <dmacmer@163.com>
2024-01-11 04:42:24 -0800

    before release lock to server ,do dorelease extent

--- lab5/lock_client_cache.cc
+++ lab5/lock_client_cache.cc
@@ -2,17 +2,18 @@
 // see lock_client.cache.h for protocol details.
 
 #include "lock_client_cache.h"
-#include "rpc.h"
-#include <sstream>
-#include <iostream>
+
 #include <stdio.h>
+
+#include <iostream>
+#include <sstream>
+
+#include "rpc.h"
 #include "tprintf.h"
 
-
-lock_client_cache::lock_client_cache(std::string xdst, 
-				     class lock_release_user *_lu)
-  : lock_client(xdst), lu(_lu)
-{
+lock_client_cache::lock_client_cache(std::string xdst,
+                                     class lock_release_user *_lu)
+    : lock_client(xdst), lu(_lu) {
   rpcs *rlsrpc = new rpcs(0);
   rlsrpc->reg(rlock_protocol::revoke, this, &lock_client_cache::revoke_handler);
   rlsrpc->reg(rlock_protocol::retry, this, &lock_client_cache::retry_handler);
@@ -24,24 +25,19 @@ lock_client_cache::lock_client_cache(std::string xdst,
   id = host.str();
 }
 
-lock_protocol::status
-lock_client_cache::acquire(lock_protocol::lockid_t lid)
-{
+lock_protocol::status lock_client_cache::acquire(lock_protocol::lockid_t lid) {
   int r;
   int ret = lock_protocol::OK;
 
   std::unique_lock<std::mutex> lck(m_mutex);
 
   auto it = m_lockMap.find(lid);
-  if(it == m_lockMap.end())
-  {
+  if (it == m_lockMap.end()) {
     it = m_lockMap.insert(std::make_pair(lid, lock_entry())).first;
   }
 
-  while(1)
-  {
-    switch(it->second.state)
-    {
+  while (1) {
+    switch (it->second.state) {
       case NONE:
         // 当客户端尝试向服务器获取锁时，状态变为ACQUIRING
         it->second.state = ACQUIRING;
@@ -51,16 +47,13 @@ lock_client_cache::acquire(lock_protocol::lockid_t lid)
         lck.lock();
 
         // 成功获得锁
-        if(ret == lock_protocol::OK)
-        {
+        if (ret == lock_protocol::OK) {
           it->second.state = LOCKED;
           return ret;
         }
         // 否则挂起在retryQueue
-        else if(ret == lock_protocol::RETRY)
-        {
-          if(!it->second.retry)
-          {
+        else if (ret == lock_protocol::RETRY) {
+          if (!it->second.retry) {
             retryQueue.wait(lck);
           }
         }
@@ -74,28 +67,21 @@ lock_client_cache::acquire(lock_protocol::lockid_t lid)
       case LOCKED:
         waitQueue.wait(lck);
         break;
-      
+
       case ACQUIRING:
-        if(!it->second.retry)
-        {
+        if (!it->second.retry) {
           waitQueue.wait(lck);
-        }
-        else
-        {
+        } else {
           it->second.retry = false;
           lck.unlock();
           ret = cl->call(lock_protocol::acquire, lid, id, r);
           lck.lock();
 
-          if(ret == lock_protocol::OK)
-          {
+          if (ret == lock_protocol::OK) {
             it->second.state = LOCKED;
             return ret;
-          }
-          else if(ret == lock_protocol::RETRY)
-          {
-            if(!it->second.retry)
-            {
+          } else if (ret == lock_protocol::RETRY) {
+            if (!it->second.retry) {
               retryQueue.wait(lck);
             }
           }
@@ -111,34 +97,29 @@ lock_client_cache::acquire(lock_protocol::lockid_t lid)
   return lock_protocol::OK;
 }
 
-lock_protocol::status
-lock_client_cache::release(lock_protocol::lockid_t lid)
-{
+lock_protocol::status lock_client_cache::release(lock_protocol::lockid_t lid) {
   int r;
   lock_protocol::status ret = lock_protocol::OK;
 
   std::unique_lock<std::mutex> lck(m_mutex);
 
   auto it = m_lockMap.find(lid);
-  if(it == m_lockMap.end())
-  {
+  if (it == m_lockMap.end()) {
     return lock_protocol::NOENT;
   }
 
-  if(it->second.revoked)
-  {
+  if (it->second.revoked) {
     it->second.state = RELEASING;
     it->second.revoked = false;
 
     lck.unlock();
+    lu->dorelease(lid);
     ret = cl->call(lock_protocol::release, lid, id, r);
     lck.lock();
 
     it->second.state = NONE;
     releaseQueue.notify_all();
-  }
-  else
-  {
+  } else {
     it->second.state = FREE;
     waitQueue.notify_one();
   }
@@ -146,50 +127,42 @@ lock_client_cache::release(lock_protocol::lockid_t lid)
   return ret;
 }
 
-rlock_protocol::status
-lock_client_cache::revoke_handler(lock_protocol::lockid_t lid, 
-                                  int &)
-{
+rlock_protocol::status lock_client_cache::revoke_handler(
+    lock_protocol::lockid_t lid, int &) {
   int r;
   int ret = rlock_protocol::OK;
 
   std::unique_lock<std::mutex> lck(m_mutex);
 
   auto it = m_lockMap.find(lid);
-  if(it == m_lockMap.end())
-  {
+  if (it == m_lockMap.end()) {
     return lock_protocol::NOENT;
   }
 
-  if(it->second.state == FREE)
-  {
+  if (it->second.state == FREE) {
     it->second.state = RELEASING;
     lck.unlock();
+    lu->dorelease(lid);
     cl->call(lock_protocol::release, lid, id, r);
     lck.lock();
 
     it->second.state = NONE;
     releaseQueue.notify_all();
-  }
-  else
-  {
+  } else {
     it->second.revoked = true;
   }
 
   return ret;
 }
 
-rlock_protocol::status
-lock_client_cache::retry_handler(lock_protocol::lockid_t lid, 
-                                 int &)
-{
+rlock_protocol::status lock_client_cache::retry_handler(
+    lock_protocol::lockid_t lid, int &) {
   int ret = rlock_protocol::OK;
 
   std::unique_lock<std::mutex> lck(m_mutex);
 
   auto it = m_lockMap.find(lid);
-  if(it == m_lockMap.end())
-  {
+  if (it == m_lockMap.end()) {
     return lock_protocol::NOENT;
   }
 
@@ -197,6 +170,3 @@ lock_client_cache::retry_handler(lock_protocol::lockid_t lid,
   retryQueue.notify_one();
   return ret;
 }
-
-
-
--- lab5/yfs_client.h
+++ lab5/yfs_client.h
@@ -2,19 +2,18 @@
 #define yfs_client_h
 
 #include <string>
-//#include "yfs_protocol.h"
-#include "extent_client.h"
+// #include "yfs_protocol.h"
 #include <vector>
 
-#include "lock_protocol.h"
+#include "extent_client.h"
 #include "lock_client.h"
-
+#include "lock_protocol.h"
 
 class yfs_client {
   extent_client *ec;
   lock_client *m_lc;
- public:
 
+ public:
   typedef unsigned long long inum;
   enum xxstatus { OK, RPCERR, NOENT, IOERR, EXIST };
   typedef int status;
@@ -38,8 +37,8 @@ class yfs_client {
  private:
   static std::string filename(inum);
   static inum n2i(std::string);
- public:
 
+ public:
   yfs_client(std::string, std::string);
 
   bool isfile(inum);
@@ -49,32 +48,30 @@ class yfs_client {
   int getdir(inum, dirinfo &);
 
   yfs_client::inum random_inum(bool isfile);
-  int create(inum, const char*, inum&);
-  int lookup(inum, const char*, inum&, bool*);
-  int readdir(inum, std::list<dirent>&);
+  int create(inum, const char *, inum &);
+  int lookup(inum, const char *, inum &, bool *);
+  int readdir(inum, std::list<dirent> &);
 
-  int setattr(inum, struct stat*);
-  int read(inum, off_t, size_t, std::string&);
-  int write(inum, off_t, size_t, const char*);
+  int setattr(inum, struct stat *);
+  int read(inum, off_t, size_t, std::string &);
+  int write(inum, off_t, size_t, const char *);
 
-  int mkdir(inum, const char*, mode_t, inum&);
-  int unlink(inum, const char*);
+  int mkdir(inum, const char *, mode_t, inum &);
+  int unlink(inum, const char *);
 };
 
 class LockGuard {
-public:
-  LockGuard(lock_client *lc, lock_protocol::lockid_t lid) : m_lc(lc), m_lid(lid)
-  {
+ public:
+  LockGuard(lock_client *lc, lock_protocol::lockid_t lid)
+      : m_lc(lc), m_lid(lid) {
     m_lc->acquire(m_lid);
   }
 
-  ~LockGuard()
-  {
-    m_lc->release(m_lid);
-  }
-private:
+  ~LockGuard() { m_lc->release(m_lid); }
+
+ private:
   lock_client *m_lc;
   lock_protocol::lockid_t m_lid;
 };
 
-#endif 
+#endif
