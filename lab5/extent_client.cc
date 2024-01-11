5c0120a25f41b47ee39f62aa76168bc29d5cb288
DaMing <dmacmer@163.com>
2024-01-11 04:53:37 -0800

    extent change virtual

--- lab5/extent_client.cc
+++ lab5/extent_client.cc
@@ -1,16 +1,17 @@
 // RPC stubs for clients to talk to extent_server
 
 #include "extent_client.h"
-#include <sstream>
-#include <iostream>
+
 #include <stdio.h>
-#include <unistd.h>
 #include <time.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <sstream>
 
 // The calls assume that the caller holds a lock on the extent
 
-extent_client::extent_client(std::string dst)
-{
+extent_client::extent_client(std::string dst) {
   sockaddr_in dstsock;
   make_sockaddr(dst.c_str(), &dstsock);
   cl = new rpcc(dstsock);
@@ -19,61 +20,31 @@ extent_client::extent_client(std::string dst)
   }
 }
 
-extent_protocol::status
-extent_client::get(extent_protocol::extentid_t eid, std::string &buf)
-{
+extent_protocol::status extent_client::get(extent_protocol::extentid_t eid,
+                                           std::string &buf) {
   extent_protocol::status ret = extent_protocol::OK;
-
-  auto it = m_dataMap.find(eid);
-  if(it==m_dataMap.end()){
-      ret = cl->call(extent_protocol::get, eid, buf);
-      m_dataMap[eid].attr.atime = time(NULL);
-      m_dataMap[eid].data = buf;
-      return ret;
-  }
-  else{
-      buf = it->second.data;
-      return ret;
-  }
+  ret = cl->call(extent_protocol::get, eid, buf);
+  return ret;
 }
 
-extent_protocol::status
-extent_client::getattr(extent_protocol::extentid_t eid, 
-		       extent_protocol::attr &attr)
-{
+extent_protocol::status extent_client::getattr(extent_protocol::extentid_t eid,
+                                               extent_protocol::attr &attr) {
   extent_protocol::status ret = extent_protocol::OK;
-  auto it = m_dataMap.find(eid);
-  if(it==m_dataMap.end()){
-      ret = extent_protocol::NOENT;
-  }
-  else{
-      attr = it->second.attr;
-      return ret;
-  }
-
   ret = cl->call(extent_protocol::getattr, eid, attr);
   return ret;
 }
 
-extent_protocol::status
-extent_client::put(extent_protocol::extentid_t eid, std::string buf)
-{
+extent_protocol::status extent_client::put(extent_protocol::extentid_t eid,
+                                           std::string buf) {
   extent_protocol::status ret = extent_protocol::OK;
   int r;
-  extent_protocol::attr attr;
-  attr.atime = attr.mtime = attr.ctime = time(NULL);
-  m_dataMap[eid].data = buf;
-  m_dataMap[eid].attr = attr;
+  ret = cl->call(extent_protocol::put, eid, buf, r);
   return ret;
 }
 
-extent_protocol::status
-extent_client::remove(extent_protocol::extentid_t eid)
-{
+extent_protocol::status extent_client::remove(extent_protocol::extentid_t eid) {
   extent_protocol::status ret = extent_protocol::OK;
   int r;
-  m_dataMap.clear();
+  ret = cl->call(extent_protocol::remove, eid, r);
   return ret;
 }
-
-
--- lab5/extent_client.h
+++ lab5/extent_client.h
@@ -4,32 +4,24 @@
 #define extent_client_h
 
 #include <string>
+
 #include "extent_protocol.h"
 #include "rpc.h"
-#include <map>
 
 class extent_client {
- private:
+ protected:
   rpcc *cl;
-  struct extent
-  {
-    std::string data;
-    extent_protocol::attr attr;
-  };
-  
-  std::map<extent_protocol::extentid_t,extent>m_dataMap;
-
 
  public:
   extent_client(std::string dst);
 
-  extent_protocol::status get(extent_protocol::extentid_t eid, 
-			      std::string &buf);
-  extent_protocol::status getattr(extent_protocol::extentid_t eid, 
-				  extent_protocol::attr &a);
-  extent_protocol::status put(extent_protocol::extentid_t eid, std::string buf);
-  extent_protocol::status remove(extent_protocol::extentid_t eid);
+  virtual extent_protocol::status get(extent_protocol::extentid_t eid,
+                                      std::string &buf);
+  virtual extent_protocol::status getattr(extent_protocol::extentid_t eid,
+                                          extent_protocol::attr &a);
+  virtual extent_protocol::status put(extent_protocol::extentid_t eid,
+                                      std::string buf);
+  virtual extent_protocol::status remove(extent_protocol::extentid_t eid);
 };
 
-#endif 
-
+#endif
